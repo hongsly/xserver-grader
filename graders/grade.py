d32357f1461377bc20824ee5a5239281db4a2301
hongsly <hongsly@gmail.com>
2014-06-27 14:44:48 -0400

    Changes to be committed: 	modified:   graders/grade.py

--- graders/grade.py
+++ graders/grade.py
@@ -1,10 +1,16 @@
 def grade(grader_path, grader_config, student_response, sandbox):
     print grader_path, grader_config, student_response, sandbox
+
+    sandbox_cmd = ' '.join(sandbox.sandbox_cmd_list())
     
     results = {
         'correct': False,
         'score': 1,
-        'tests': [('a','b','c','d','e')],
+        'tests': [
+	('short description 1','long description 1',False,'expected1','actual1'),
+	('short 2', 'sandbox_cmd: '+sandbox_cmd,False,'grader_path: '+str(grader_path), 'student_response: '+student_response),
+	('short 3','grader_config: '+str(grader_config), True, 'e3',str(eval('2+3'))),
+	],
         'errors': []
     }
     return results
